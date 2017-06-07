73477e62d92517bfd5b1b30570860f49ce0351d3
Dylan Jay <software@pretaweb.com>
2017-06-07 14:16:27 +0700

    now won't merge non 200 responses.

--- merge206.py
+++ merge206.py
@@ -16,16 +16,16 @@ from collections import OrderedDict
 import re
 import sys
 import datetime
+import apache_log_parser
+from StringIO import StringIO
+from docopt import docopt
 
 __author__ = 'dylanjay'
 
-import apache_log_parser
-from StringIO import StringIO
 
  # supported log file formats
 APACHE_COMBINED="%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\""
 APACHE_COMMON="%h %l %u %t \"%r\" %>s %b"
-
 
 # similar request keys
 
@@ -34,104 +34,128 @@ KEYS = ['request_header_referer', 'remote_user', 'request_header_user_agent', 'r
  #'response_bytes_clf']
 
 
-from docopt import docopt
-
-# plan:
-
-def hash_entry(data):
-    return tuple( data[key] for key in KEYS )
-
-
 
 def merge_recent_entries(input, output, pattern=APACHE_COMBINED, delay=600):
     """
     If we get several requests in row that look like from the same user then we will merge
 
-    >>> log = StringIO('''
+    >>> merge_recent_entries(StringIO('''
     ... 1.1.1.1 - - [26/Nov/2015:04:58:59 +0000] "GET /blah HTTP/1.1" 200 2 "http://foo.com" "mybrowser"
     ... 1.1.1.1 - - [26/Nov/2015:04:59:05 +0000] "GET /blah HTTP/1.1" 206 742750 "http://foo.com" "mybrowser"
     ... 1.1.1.1 - - [26/Nov/2015:04:59:07 +0000] "GET /blah HTTP/1.1" 206 69475 "http://foo.com" "mybrowser"
     ... 1.1.1.1 - - [26/Nov/2015:04:59:08 +0000] "GET /blah HTTP/1.1" 206 3939 "http://foo.com" "mybrowser"
-    ... '''.strip())
-    >>> merge_recent_entries(log, sys.stdout)
+    ... '''.strip()), sys.stdout)
     1.1.1.1 - - [26/Nov/2015:04:58:59 +0000] "GET /blah HTTP/1.1" 200 816166 "http://foo.com" "mybrowser"
 
+    Interleving requests will get merged
+    >>> merge_recent_entries(StringIO('''
+    ... 1.1.1.1 - - [26/Nov/2015:04:58:59 +0000] "GET /blah HTTP/1.1" 200 2 "http://foo.com" "mybrowser"
+    ... 1.1.1.2 - - [26/Nov/2015:04:59:05 +0000] "GET /blah HTTP/1.1" 200 742750 "http://foo.com" "mybrowser"
+    ... 1.1.1.1 - - [26/Nov/2015:04:59:07 +0000] "GET /blah HTTP/1.1" 206 69475 "http://foo.com" "mybrowser"
+    ... 1.1.1.2 - - [26/Nov/2015:04:59:08 +0000] "GET /blah HTTP/1.1" 206 3939 "http://foo.com" "mybrowser"
+    ... '''.strip()), sys.stdout)
+    1.1.1.1 - - [26/Nov/2015:04:58:59 +0000] "GET /blah HTTP/1.1" 200 69477 "http://foo.com" "mybrowser"
+    1.1.1.2 - - [26/Nov/2015:04:59:05 +0000] "GET /blah HTTP/1.1" 200 746689 "http://foo.com" "mybrowser"
+
     If from a different client IP then we won't merge
-    >>> log = StringIO('''
+    >>> merge_recent_entries(StringIO('''
     ... 1.1.1.1 - - [26/Nov/2015:04:58:59 +0000] "GET /blah HTTP/1.1" 200 2 "http://foo.com" "mybrowser"
     ... 1.1.1.2 - - [26/Nov/2015:04:59:05 +0000] "GET /blah HTTP/1.1" 206 742750 "http://foo.com" "mybrowser"
-    ... '''.strip())
-    >>> merge_recent_entries(log, sys.stdout)
+    ... '''.strip()), sys.stdout)
     1.1.1.1 - - [26/Nov/2015:04:58:59 +0000] "GET /blah HTTP/1.1" 200 2 "http://foo.com" "mybrowser"
     1.1.1.2 - - [26/Nov/2015:04:59:05 +0000] "GET /blah HTTP/1.1" 206 742750 "http://foo.com" "mybrowser"
 
     If from a different browser then we won't merge
-    >>> log = StringIO('''
+    >>> merge_recent_entries(StringIO('''
     ... 1.1.1.1 - - [26/Nov/2015:04:58:59 +0000] "GET /blah HTTP/1.1" 200 2 "http://foo.com" "mybrowser"
     ... 1.1.1.1 - - [26/Nov/2015:04:59:05 +0000] "GET /blah HTTP/1.1" 206 742750 "http://foo.com" "notmybrowser"
-    ... '''.strip())
-    >>> merge_recent_entries(log, sys.stdout)
+    ... '''.strip()), sys.stdout)
     1.1.1.1 - - [26/Nov/2015:04:58:59 +0000] "GET /blah HTTP/1.1" 200 2 "http://foo.com" "mybrowser"
     1.1.1.1 - - [26/Nov/2015:04:59:05 +0000] "GET /blah HTTP/1.1" 206 742750 "http://foo.com" "notmybrowser"
 
     A 404 or 500 won't get merged
-    >>> log = StringIO('''
+    >>> merge_recent_entries(StringIO('''
     ... 1.1.1.1 - - [26/Nov/2015:04:58:59 +0000] "GET /blah HTTP/1.1" 404 2 "http://foo.com" "mybrowser"
-    ... 1.1.1.1 - - [26/Nov/2015:04:58:59 +0000] "GET /blah2 HTTP/1.1" 500 2 "http://foo.com" "mybrowser"
+    ... 1.1.1.1 - - [26/Nov/2015:04:59:01 +0000] "GET /blah2 HTTP/1.1" 500 2 "http://foo.com" "mybrowser"
     ... 1.1.1.1 - - [26/Nov/2015:04:59:05 +0000] "GET /blah HTTP/1.1" 206 742750 "http://foo.com" "mybrowser"
-    ... 1.1.1.1 - - [26/Nov/2015:04:59:05 +0000] "GET /blah2 HTTP/1.1" 206 742750 "http://foo.com" "mybrowser"
-    ... '''.strip())
-    >>> merge_recent_entries(log, sys.stdout)
+    ... 1.1.1.1 - - [26/Nov/2015:04:59:06 +0000] "GET /blah2 HTTP/1.1" 206 742750 "http://foo.com" "mybrowser"
+    ... '''.strip()), sys.stdout)
     1.1.1.1 - - [26/Nov/2015:04:58:59 +0000] "GET /blah HTTP/1.1" 404 2 "http://foo.com" "mybrowser"
-    1.1.1.1 - - [26/Nov/2015:04:59:05 +0000] "GET /blah2 HTTP/1.1" 500 742750 "http://foo.com" "mybrowser"
+    1.1.1.1 - - [26/Nov/2015:04:59:01 +0000] "GET /blah2 HTTP/1.1" 500 2 "http://foo.com" "mybrowser"
     1.1.1.1 - - [26/Nov/2015:04:59:05 +0000] "GET /blah HTTP/1.1" 206 742750 "http://foo.com" "mybrowser"
-    1.1.1.1 - - [26/Nov/2015:04:59:05 +0000] "GET /blah2 HTTP/1.1" 206 742750 "http://foo.com" "mybrowser"
+    1.1.1.1 - - [26/Nov/2015:04:59:06 +0000] "GET /blah2 HTTP/1.1" 206 742750 "http://foo.com" "mybrowser"
+
+    If they are too far apart then it won't get merged
+    >>> merge_recent_entries(StringIO('''
+    ... 1.1.1.1 - - [26/Nov/2015:04:41:59 +0000] "GET /blah HTTP/1.1" 200 2 "http://foo.com" "mybrowser"
+    ... 1.1.1.1 - - [26/Nov/2015:04:59:05 +0000] "GET /blah HTTP/1.1" 206 742750 "http://foo.com" "mybrowser"
+    ... '''.strip()), sys.stdout)
+    1.1.1.1 - - [26/Nov/2015:04:41:59 +0000] "GET /blah HTTP/1.1" 200 2 "http://foo.com" "mybrowser"
+    1.1.1.1 - - [26/Nov/2015:04:59:05 +0000] "GET /blah HTTP/1.1" 206 742750 "http://foo.com" "mybrowser"
+
+    Two 200 requests won't get merged
+    >>> merge_recent_entries(StringIO('''
+    ... 1.1.1.1 - - [26/Nov/2015:04:58:59 +0000] "GET /blah HTTP/1.1" 200 2 "http://foo.com" "mybrowser"
+    ... 1.1.1.1 - - [26/Nov/2015:04:59:05 +0000] "GET /blah HTTP/1.1" 200 742750 "http://foo.com" "mybrowser"
+    ... '''.strip()), sys.stdout)
+    1.1.1.1 - - [26/Nov/2015:04:58:59 +0000] "GET /blah HTTP/1.1" 200 2 "http://foo.com" "mybrowser"
+    1.1.1.1 - - [26/Nov/2015:04:59:05 +0000] "GET /blah HTTP/1.1" 200 742750 "http://foo.com" "mybrowser"
     """
 
+    if not pattern:
+        pattern = APACHE_COMBINED
+    delay = datetime.timedelta(seconds=delay)
+
+    line_parser=apache_log_parser.make_parser(pattern)
 
     # keep last 1min worth of log entries and look them up based on if its the same
     # request repeated
     buffer = OrderedDict()
 
-    
-    if not pattern:
-        pattern = APACHE_COMBINED
-    line_parser=apache_log_parser.make_parser(pattern)
+
+    def hash_entry(data):
+        # we need to make sure we only have a single mergable item in the buffer at anyone time
+        # We can only merge 200 and 206 so they get the same hash.
+        keys = KEYS + (['status'] if data['status'] not in ['200','206'] else [])
+        return tuple( data[key] for key in  keys)
+
+
+
+    #import pdb; pdb.set_trace()
     for line in input.readlines():
         data = line_parser(line)
-        #if '01:10:30' in line:
-        #    import pdb; pdb.set_trace()
         # get rid of too old entries at the end of our buffer
-        for oldline, oldtime in buffer.values():
-            if data['time_received_utc_datetimeobj'] - oldtime > datetime.timedelta(seconds=delay):
+        for oldline, oldtime, _ in buffer.values():
+            if data['time_received_utc_datetimeobj'] - oldtime > delay:
                 output.write(oldline)
                 buffer.popitem(last=False)
             else:
                 break
+
         hash = hash_entry(data)
-        if hash not in buffer or data['status'] != '206':
-            buffer[hash] = (line, data['time_received_utc_datetimeobj'])
+        if hash not in buffer:
+            buffer[hash] = (line, data['time_received_utc_datetimeobj'], data)
             continue
 
-        oldline,oldtime = buffer[hash]
+        # we have a line in the buffer we can merge with
+        oldline, oldtime, olddata = buffer[hash]
         del buffer[hash]
-        olddata = line_parser(oldline)
-        if olddata['status'] not in ['200', '206']:
-            buffer[hash] = (line, data['time_received_utc_datetimeobj'])
-            continue
 
         # Combine the lines
         #update the response size
         oldbytes = olddata['response_bytes_clf']
         newbytes = data['response_bytes_clf']
         bytes = int(oldbytes) + int(newbytes)
+        data['response_bytes_clf'] = str(bytes)
         # TODO: bit of a hack, what if this occurs twice?
         line, count = re.subn(" {0} ".format(oldbytes), " {0} ".format(bytes), oldline )
         if count == 0 or count > 1:
             raise Exception("error trying to merge response bytes")
-        buffer[hash] = (line, data['time_received_utc_datetimeobj'])
+        # Important we add this back in with the timestamp of the latest in the merge
+        # not the original.
+        buffer[hash] = (line, data['time_received_utc_datetimeobj'], data)
 
-    for line, timestamp in buffer.values():
+    for line, _, _ in buffer.values():
             output.write(line)
 
 def main():
